6c4eca18988607bd31804562d06f22f5a596a9fe
DinDin <din.din@appimc.com>
2019-03-17 15:16:55 +0800

    initial project

---
+++ SwiftMenuPages/SwiftMenuPages.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMenuPages.h
+//  SwiftMenuPages
+//
+//  Created by DinDin on 2019/3/17.
+//  Copyright © 2019 DinDin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftMenuPages.
+FOUNDATION_EXPORT double SwiftMenuPagesVersionNumber;
+
+//! Project version string for SwiftMenuPages.
+FOUNDATION_EXPORT const unsigned char SwiftMenuPagesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMenuPages/PublicHeader.h>
+
+
